46bdba0d5d434e830b331b3094bf0223f6b13283
Blocku <126176292+Blocku@users.noreply.github.com>
2024-09-23 20:57:45 +0500

    nothing new

--- mod.ts
+++ mod.ts
@@ -1,5 +1,10 @@
-import { Bot } from "grammy";
+import { Bot } from "https://deno.land/x/grammy@v1.30.0/mod.ts";
 
 
+export const bot = new Bot('7762728645:AAHxsNQDL_8VyN58_vdlCZ3VJOUtwd85INc');
 
-export const bot = new Bot(Deno.env.get("BOT_TOKEN") || "");
+bot.command('start', (ctx) => ctx.reply('Добро пожаловать'))
+
+bot.on("message", (ctx) => ctx.reply("Получил другое сообщение!"));
+
+bot.start()
